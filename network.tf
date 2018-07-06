3a7d5b8619ce65bdfc524b04bef5f94a0b2600cf
nitzanweisberg <37505243+nitzanweisberg@users.noreply.github.com>
2018-07-07 00:19:28 +0300

    VPC and EC2

---
+++ network.tf
@@ -0,0 +1,42 @@
+resource "aws_vpc" "TF_nitzan" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support = true
+  tags {
+    Name = "Terraform VPC example"
+  }
+}
+
+resource "aws_subnet" "subnet1" {
+  cidr_block = "${cidrsubnet(aws_vpc.TF_nitzan.cidr_block, 3, 1)}"
+  vpc_id = "${aws_vpc.TF_nitzan.id}"
+  availability_zone = "eu-west-3a"
+  tags {
+    Name = "TF_Subnet1"
+  }
+}
+
+resource "aws_subnet" "subnet2" {
+  cidr_block = "${cidrsubnet(aws_vpc.TF_nitzan.cidr_block, 2, 2)}"
+  vpc_id = "${aws_vpc.TF_nitzan.id}"
+  availability_zone = "eu-west-3b"
+  tags {
+    Name = "TF_Subnet2"
+  }
+}
+resource "aws_security_group" "TF_SG" {
+  vpc_id = "${aws_vpc.TF_nitzan.id}"
+  tags {
+    Name = "TF_SG"
+  }
+  ingress {
+      cidr_blocks = [
+        "${aws_vpc.TF_nitzan.cidr_block}"
+      ]
+
+    from_port = 80
+    protocol = "tcp"
+    to_port = 80
+  }
+
+}
---
+++ servers.tf
@@ -0,0 +1,34 @@
+
+
+
+data "aws_ami" "amzn-linux" {
+  most_recent = true
+
+  filter {
+    name = "name"
+    values = ["amzn2-ami-hvm-2.0.*-x86_64-gp2"]
+  }
+  filter {
+    name = "virtualization-type"
+    values = ["hvm"]
+  }
+  owners = ["137112412989"]
+}
+
+#data "aws_security_group" "TF-SG" {
+#  id = "sg-4c2ffc24"
+#}
+
+
+resource "aws_instance" "TF-Server" {
+  ami = "${data.aws_ami.amzn-linux.id}"
+  instance_type = "t2.micro"
+  subnet_id = "${aws_subnet.subnet1.id}"
+  key_name = "Paris-sandbox"
+  security_groups = [
+    "sg-4c2ffc24" ]
+  tags {
+    Name = "TF-Server"
+  }
+
+}
